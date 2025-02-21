a8b2ca23670858020e9cb343806264045fb8d677
Haleema-Malik <haleemamalik445@gmail.in>
2025-02-22 04:51:29 +0500

    Add Responsiveness

---
+++ my-project/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Counter from './assets/Components/Counter'
+import Background from '../../Word_count/Components/Background'
+
+function App() {
+  return (
+    <>
+    
+    <Background/>
+    </>
+  )
+}
+
+export default App
--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Counter() {
      return (
           <div className='flex flex-col items-center justify-center'>
-               <textarea id="textInput" rows={4} cols={30} className='border-x-8 border-y-2 border-blue-950 bg-slate-100 rounded-3xl text-3xl text-blue-500 font-semibold flex text-center outline-none mt-12 pt-24 placeholder-slate-300 placeholder:font-medium' placeholder='Enter Your Text Here'></textarea>
+               <textarea id="textInput" rows={4} cols={30} className='resize-y max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl border-x-8 border-y-2 border-blue-950 bg-slate-100 rounded-3xl text-3xl text-blue-500 font-semibold flex text-center outline-none mt-12 pt-24 mx-6 placeholder-slate-300 placeholder:font-medium' placeholder='Enter Your Text Here'></textarea>
                <div className='flex flex-col text-center text-2xl text-blue-950 font-bold space-y-3 mt-5'>
                     <div className='flex space-x-7'>
                          <p>WORDS : <span id='wordCount' className='text-blue-500'>0</span></p>
